100f1e855249e99b204e51997e84deee5d8207a3
kcgpatrika-maker <kcg.patrika@gmail.com>
2026-05-28 17:40:31 +0530

    Create rail-wiki.js

---
+++ rail-wiki.js
@@ -0,0 +1,38 @@
+const railWikiData = [
+
+  {
+    title:"🇮🇳 Indian Railways",
+    page:"Indian_Railways"
+  },
+
+  {
+    title:"🚆 North Western Railway",
+    page:"North_Western_Railway"
+  },
+
+  {
+    title:"🚆 Northern Railway",
+    page:"Northern_Railway_(India)"
+  },
+
+  {
+    title:"🚆 Western Railway",
+    page:"Western_Railway_zone"
+  },
+
+  {
+    title:"🚆 Central Railway",
+    page:"Central_Railway_zone"
+  },
+
+  {
+    title:"📍 Jaipur Division",
+    page:"Jaipur_railway_division"
+  },
+
+  {
+    title:"📍 Ajmer Division",
+    page:"Ajmer_railway_division"
+  }
+
+];
